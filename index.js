1305f8e0a9359037d07aab129ce6954856be8b1a
William Venner <william@venner.io>
2018-09-09 20:53:24 +0000

    2.0.1: Moved some variables

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const arg_parser_regex = /(?:"(.+?)" |'(.+?)' |(.+?) )/;
+
 module.exports = function(cmd_prefix, input) {
 	// check whether the input is a command
 	if (input[0] === cmd_prefix) {
-		let cmd;
-		let args = [];
-		
 		// parse every space
 		let unparsed_args = input.split(" ");
-		cmd = unparsed_args[0].substr(cmd_prefix.length);
+		// extract our command
+		let cmd = unparsed_args[0].substr(cmd_prefix.length);
 		
 		// if the command itself is empty, the input was not a command
 		if (cmd.length === 0) return false;
 		
+		let args = [];
 		// check whether there's actually arguments
 		if (unparsed_args.length > 1) {
 			// remove the command from our arguments array
