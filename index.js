4f60085c8f8f061ec41a90e37fc9e35757c2b330
William Venner <william@venner.io>
2018-09-12 00:58:17 +0000

    2.1.2: fixed indentation + comment changes

--- index.js
+++ index.js
@@ -6,11 +6,13 @@
 const arg_parser_regex = /(?:"(.+?)" |'(.+?)' |(.+?) )/;
 
 module.exports = function(cmd_prefix, input) {
-    // check whether our input is a command...
-    // firstly check whether the user prefixed their command with the command prefix
-    // secondly check whether the user hasn't just typed the command prefix
+	
+	// check whether our input is a command...
+	// firstly check whether the user prefixed their command with the command prefix
+	// secondly check whether the user hasn't just typed the command prefix
+	
 	if (input.startsWith(cmd_prefix) && input.trimRight() !== cmd_prefix) {
-	    // initialise our arguments list
+		// initialise our arguments list
 		let args = [];
 		
 		// extract the command + args from the input
@@ -26,20 +28,20 @@ module.exports = function(cmd_prefix, input) {
 		let unparsed_args = cmd.substr(first_space + 1) + " ";
 		// start a while loop to iterate through our Regex matches
 		while (true) {
-		    // execute our Regex
-		    let exec = arg_parser_regex.exec(unparsed_args);
-		    // if there's no matches left, we've parsed every argument
-		    // so we can break out of the loop
-		    if (!exec) break;
-		    
-		    // extract the argument from our three matched Regex groups
-		    // third group is most common (no quotations)
-		    // first group is 2nd most common (double quotes)
-		    // second group is least common (single quotes)
-		    args.push(exec[3] || exec[1] || exec[2]);
-		    
-		    // remove the parsed argument from our unparsed arguments
-		    unparsed_args = unparsed_args.substr(exec[0].length);
+			// execute our Regex
+			let exec = arg_parser_regex.exec(unparsed_args);
+			// if there's no matches left, we've parsed every argument
+			// so we can break out of the loop
+			if (!exec) break;
+			
+			// extract the argument from our three matched Regex groups
+			// third group is most common (no quotations)
+			// first group is 2nd most common (double quotes)
+			// second group is least common (single quotes)
+			args.push(exec[3] || exec[1] || exec[2]);
+			
+			// remove the parsed argument from our unparsed arguments
+			unparsed_args = unparsed_args.substr(exec[0].length);
 		}
 		
 		// return the command and its arguments
@@ -48,6 +50,8 @@ module.exports = function(cmd_prefix, input) {
 			args
 		};
 	}
+	
 	// not a command
 	return false;
+	
 };
