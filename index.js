9800cc2c30e20abe89ba9878396ef14fbfc25a9a
William Venner <william@venner.io>
2018-09-12 00:37:37 +0000

    2.1.0: Fixed a load of bugs, massively optimised code, added better comments

--- index.js
+++ index.js
@@ -1,36 +1,53 @@
+// our argument parsing Regex
+// requires a space to be padded to the end of the string to capture all arguments
+// first group is a double quoted argument
+// second group is a single quoted argument
+// third group is a non-quoted argument (this has to be the last group)
 const arg_parser_regex = /(?:"(.+?)" |'(.+?)' |(.+?) )/;
 
 module.exports = function(cmd_prefix, input) {
-	// check whether the input is a command
-	if (input[cmd_prefix.length] === cmd_prefix) {
-		// parse every space
-		let unparsed_args = input.split(" ");
-		// extract our command
-		let cmd = unparsed_args[0].substr(cmd_prefix.length);
-		
-		// if the command itself is empty, the input was not a command
-		if (cmd.length === 0) return false;
-		
+    // check whether our input is a command...
+    // firstly check whether the user prefixed their command with the command prefix
+    // secondly check whether the user hasn't just typed the command prefix
+	if (input.startsWith(cmd_prefix) && input.trimRight() !== cmd_prefix) {
+	    // initialise our arguments list
 		let args = [];
-		// check whether there's actually arguments
-		if (unparsed_args.length > 1) {
-			// remove the command from our arguments array
-			unparsed_args = unparsed_args.splice(1);
-			// start a while loop so we can Regex exec each argument
-			while (unparsed_args.length > 0) {
-				// our Regex requires a space at the end of every argument, so add one if it's not there
-				let exec = arg_parser_regex.exec(unparsed_args.join(" ").trimRight() + " ");
-				// extract argument from our Regex groups
-				let arg = exec[1] || exec[2] || exec[3];
-				
-				// add it to our argument list
-				args.push(arg);
-				
-				unparsed_args = unparsed_args.splice(1);
-			}
+		
+		// extract the command + args from the input
+		let cmd = input.substr(cmd_prefix.length);
+		
+		// find the first space in the input
+		let first_space = cmd.indexOf(" ");
+		// if there's no space then there's no arguments,
+		// so we can just return our command and its empty arguments
+		if (first_space === -1) return {cmd, args};
+		
+		// extract the arguments out of our command
+		let unparsed_args = cmd.substr(first_space + 1) + " ";
+		// start a while loop to iterate through our Regex matches
+		while (true) {
+		    // execute our Regex
+		    let exec = arg_parser_regex.exec(unparsed_args);
+		    // if there's no matches left, we've parsed every argument
+		    // so we can break out of the loop
+		    if (!exec) break;
+		    
+		    // extract the argument from our three matched Regex groups
+		    // third group is most common (no quotations)
+		    // first group is 2nd most common (double quotes)
+		    // second group is least common (single quotes)
+		    args.push(exec[3] || exec[1] || exec[2]);
+		    
+		    // remove the parsed argument from our unparsed arguments
+		    unparsed_args = unparsed_args.substr(exec[0].length);
 		}
 		
-		return [cmd, args];
+		// return the command and its arguments
+		return {
+			cmd: cmd.substr(0, first_space),
+			args
+		};
 	}
+	// not a command
 	return false;
 };
